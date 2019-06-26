e8596a7eb997bba83e52d310b25235bd489eec8b
Calvin Webster <calweb@gmail.com>
2019-06-26 11:49:12 -0400

    refactored routes into their own file

--- api/routes/students.js
+++ api/routes/students.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const Student = require('../models/Student')
-const router = express.Router
+const router = express.Router()
 
+// /students/
 router
 	.route('/')
 	.get(function(request, response) {
@@ -9,12 +10,40 @@ router
 			response.json({ status: 'ok', data: students })
 		})
 	})
-	.post(function(request, response) {})
+	.post(function(request, response) {
+		const rawStudent = request.body
 
+		const newStudent = new Student(rawStudent)
+
+		newStudent.save()
+
+		response.json({ status: 'ok', newStudent })
+	})
+
+// /students/:studentId
 router
 	.route('/:studentId')
-	.get(function(request, response) {})
-	.put(function(request, response) {})
-	.delete(function(request, response) {})
+	.get(function(request, response) {
+		Student.findById(request.params.studentId).then(function(foundStudent) {
+			response.json(foundStudent)
+		})
+	})
+	.put(function(request, response) {
+		Student.findById(request.params.studentId).then(function(foundStudent) {
+			foundStudent.name = request.body.name
+			foundStudent.age = request.body.age
+			foundStudent.photoUrl = request.body.photoUrl
+			foundStudent.bio = request.body.bio
+
+			foundStudent.save()
+
+			response.json(foundStudent)
+		})
+	})
+	.delete(function(request, response) {
+		Student.findByIdAndDelete(request.params.studentId).then(function(res) {
+			response.json({ status: 'ok', res: res })
+		})
+	})
 
 module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,60 +1,17 @@
 const express = require('express')
-const Student = require('./models/Student')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-
+const StudentRoutes = require('./routes/students')
 mongoose.connect('mongodb://localhost:27017/students', { useNewUrlParser: true })
 
 const app = express()
 
 app.use(bodyParser())
-
-// Main GET / route
 app.get('/', function(request, response) {
 	console.log('We live in the GET route')
 	response.json({ status: 'ok', message: 'Welcome to my server.' })
 })
-
-app.get('/students', function(request, response) {
-	Student.find({}).then(function(students) {
-		response.json({ status: 'ok', data: students })
-	})
-})
-
-app.post('/students', function(request, response) {
-	const rawStudent = request.body
-
-	const newStudent = new Student(rawStudent)
-
-	newStudent.save()
-
-	response.json({ status: 'ok', newStudent })
-})
-
-app.get('/students/:studentId', function(request, response) {
-	Student.findById(request.params.studentId).then(function(foundStudent) {
-		response.json(foundStudent)
-	})
-})
-
-app.put('/students/:studentId', function(request, response) {
-	Student.findById(request.params.studentId).then(function(foundStudent) {
-		foundStudent.name = request.body.name
-		foundStudent.age = request.body.age
-		foundStudent.photoUrl = request.body.photoUrl
-		foundStudent.bio = request.body.bio
-
-		foundStudent.save()
-
-		response.json(foundStudent)
-	})
-})
-
-app.delete('/students/:studentId', function(request, response) {
-	Student.findByIdAndDelete(request.params.studentId).then(function(res) {
-		response.json({ status: 'ok', res: res })
-	})
-})
+app.use('/students', StudentRoutes)
 
 app.listen(8000, function() {
 	console.log('Server is listening on PORT 8000 -- hello everyone')
